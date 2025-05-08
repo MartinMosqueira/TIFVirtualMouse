af32b0d8ca10656ce566da23c726ac5dfe3672ac
MartinMosqueira <tinchomosqueira@gmail.com>
2025-05-08 20:18:19 -0300

    feat(controllers): :sparkles: new filter for move cursor Added new filter EMA for adaptative move cursor.

---
+++ AdaptiveCursor.py
@@ -0,0 +1,53 @@
+import autopy, time, math
+
+class AdaptiveCursor:
+    def __init__(self, alpha_min, alpha_max, speed_sens):
+        self.alpha_min = alpha_min
+        self.alpha_max = alpha_max
+        self.speed_sens = speed_sens
+        # get screen size
+        self.screen_w, self.screen_h = autopy.screen.size()
+
+        self.prev_x = self.prev_y = None
+        self.prev_t = None
+        self.prev_sx = self.prev_sy = None
+
+    def move(self, x, y):
+        # off-screen detection
+        if not (0 <= x <= 1 and 0 <= y <= 1):
+            return
+
+        now = time.time()
+        if self.prev_x is None or self.prev_y is None:
+            # init position
+            self.prev_x, self.prev_y = x, y
+            self.prev_t = now
+            self.prev_sx, self.prev_sy = x, y
+            autopy.mouse.move(int(x*self.screen_w), int(y*self.screen_h))
+            return
+
+        # calculate the exact frame rate
+        dt = now - self.prev_t
+        dx, dy = x - self.prev_x, y - self.prev_y
+        dist = math.hypot(dx, dy)
+        speed = dist / dt if dt > 0 else 0
+
+        # adjusts smoothed based on frame rate
+        frac = speed / (speed + self.speed_sens)
+
+        # interpolates the value between min and max
+        alpha = self.alpha_min + (self.alpha_max - self.alpha_min) * frac
+
+        # EMA filter
+        sx = alpha * x + (1 - alpha) * self.prev_sx
+        sy = alpha * y + (1 - alpha) * self.prev_sy
+
+        # move cursor
+        screen_x = int(sx * self.screen_w)
+        screen_y = int(sy * self.screen_h)
+        autopy.mouse.move(screen_x, screen_y)
+
+        # update values
+        self.prev_x, self.prev_y = x, y
+        self.prev_sx, self.prev_sy = sx, sy
+        self.prev_t = now
