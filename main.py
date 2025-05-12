eb4ee4eb2c88b8a2b3d1ff62c069877d78401049
MartinMosqueira <tinchomosqueira@gmail.com>
2025-05-12 16:55:18 -0300

    fix(structure): :truck: upadte path main model

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from HandTracker import HandTracker
 
 # ==== Ejecutar el tracker ====
 if __name__ == '__main__':
-    model_path = 'hand_landmarker.task'
+    model_path = 'model/hand_landmarker.task'
     tracker = HandTracker(model_path)
     tracker.run()
