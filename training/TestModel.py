18b031af3e9fd6af6fc42ac7503749f68af57b42
MartinMosqueira <tinchomosqueira@gmail.com>
2025-06-13 15:54:45 -0300

    ✅ New function for test multiclass model

--- training/TestModel.py
+++ training/TestModel.py
@@ -26,12 +26,28 @@ class TestModel:
         prob = float(self.modelGesture.predict(seq, verbose=0)[0, 0])
         print(f"Probabilidad de gesto: {prob:.3f}")
 
+    def test_model_LSTM_multiclass(self, coords_list):
+        arr = np.array(coords_list)          # shape (30, 63)
+        seq = arr[None, ...]                # shape (1, 30, 63)
+        prob = self.modelGesture.predict(seq, verbose=0)[0]
+        print(f"Probabilidades de clases: {prob}")
+        predicted_class = np.argmax(prob)
+
+        if predicted_class == 0:
+            print("Gesto: Ninguno")
+        elif predicted_class == 1:
+            print("Gesto: Click Izquierdo")
+        elif predicted_class == 2:
+            print("Gesto: Click Derecho")
+        elif predicted_class == 3:
+            print("Gesto: Scroll")
+
 if __name__ == '__main__':
-    model_path = ''
+    model_path = '../model/gesture/'
     test_model = TestModel(model_path)
 
     # Read the CSV file
     coords_list = test_model.read_csv('')
 
     # Test the model with the read coordinates
-    test_model.test_model_LSTM(coords_list)
+    test_model.test_model_LSTM_multiclass(coords_list)
